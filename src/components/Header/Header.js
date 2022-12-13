42be6a416bebd0a9e6e827eb204e1d01a6102515
lalarahimli <114071420+lalarahimli@users.noreply.github.com>
2022-12-13 15:29:20 +0400

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+
+const  Header =()=> {
+
+        return (
+            <header className="baslig">
+                <h1 className="baslig__title">
+                    MustSee
+                </h1>
+            </header>
+        );
+  
+}
+ 
+export default Header;
